f473524f10ecfa78056162d3301bc6bf363f9c7a
Shouda Ren <2521509R@student.gla.ac.uk>
2021-07-28 13:08:34 +0100

    chapter 4 test finished

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    # Refer to the TwD book for more information on how this updated view works.
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Shouda'}
-    return render(request, 'rango/about.html', context=context_dict)
+    return render(request, 'rango/about.html')
