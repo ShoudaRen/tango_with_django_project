7026fbd74bc1bdd33f81c9d89922a2b8287a4bd8
Shouda Ren <2521509R@student.gla.ac.uk>
2021-07-29 12:55:32 +0100

    chapter6 test finished

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
@@ -24,6 +23,5 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
 
-    
     def __str__(self):
         return self.title
