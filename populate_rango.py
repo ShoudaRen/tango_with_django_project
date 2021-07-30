f2809e0bcf9e0ff3c99f17e5839e01b6d91cdc09
Shouda Ren <2521509R@student.gla.ac.uk>
2021-07-30 04:19:54 +0100

    update base.html to abstract

--- populate_rango.py
+++ populate_rango.py
@@ -5,38 +5,37 @@ import django
 django.setup()
 from rango.models import Category, Page
 
-# For an explanation of what is going on here, please refer to the TwD book.
 
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/',
-         'views': 114,},
+         'views': 234,},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/',
-         'views': 53},
+         'views': 144},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/',
-         'views': 20} ]
+         'views': 23} ]
     
     django_pages = [
         {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-         'views': 32},
+         'views': 33},
         {'title':'Django Rocks',
          'url':'http://www.djangorocks.com/',
-         'views': 12},
+         'views': 22},
         {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/',
-         'views': 1258} ]
+         'views': 123} ]
     
     other_pages = [
         {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/',
-         'views': 54},
+         'views': 213},
         {'title':'Flask',
          'url':'http://flask.pocoo.org',
-         'views': 64} ]
+         'views': 124} ]
     
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -65,7 +64,7 @@ def add_cat(name, views=0, likes=0):
     c.save()
     return c
 
-# Start execution here!
+# main method 
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
