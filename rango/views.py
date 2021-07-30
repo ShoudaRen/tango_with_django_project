a981a826d9d423addc6bbdc8223f1e6acbfef14b
Shouda Ren <2521509R@student.gla.ac.uk>
2021-07-30 10:50:21 +0100

    login system done

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,8 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 
 def index(request):
@@ -119,3 +121,28 @@ def register(request):
         profile_form = UserProfileForm()
     return render(request,'rango/register.html',context = {'user_form': user_form, 'profile_form': profile_form,
 'registered': registered})
+
+
+# login 
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                # Is the account active? It could have been disabled.
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'rango/login.html')
